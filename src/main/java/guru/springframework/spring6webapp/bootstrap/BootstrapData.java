e360f85958c7eda1dc5058e5708f004e67f19de1
Cleberson <pauluci.cleberson@gmail.com>
2023-10-02 22:09:10 -0300

    Created Publisher Entity and repository

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -55,7 +59,23 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
 
+        Publisher pearson = new Publisher();
+        pearson.setPublisherName("Pearson");
+        pearson.setAddress("Av. Francisco Matarazzo, 1400 - Água Branca");
+        pearson.setState("SP");
+        pearson.setZipCode("05001-100");
+        pearson.setCity("São Paulo");
+        Publisher pearsonSaved = publisherRepository.save(pearson);
 
+        Publisher apress = new Publisher();
+        apress.setPublisherName("Apress");
+        apress.setAddress("Apress Media, LLC One New York Plaza, Suite 4600");
+        apress.setState("NY");
+        apress.setZipCode("10004-1562");
+        apress.setCity("New York");
+        Publisher apressSaved = publisherRepository.save(apress);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
 
